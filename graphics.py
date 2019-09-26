1228c76dc74b9d5a22dd2560c5a09b9e4d1f4213
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2019-09-26 15:03:00 +0200

    Add files via upload

--- graphics.py
+++ graphics.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter, math
 
 class Engine3D:
     def __init__(self, points, triangles, width=1000, height=700, distance=6, scale=100):
@@ -8,6 +8,7 @@ class Engine3D:
         self.scale = scale
         
         self.window = tkinter.Tk()
+        self.window.title('3D')
         self.image = tkinter.Canvas(self.window, width=self.width, height=self.height, bg='White')
         self.image.pack()
         
@@ -15,6 +16,33 @@ class Engine3D:
         self.points = points
         self.triangles = triangles
 
+    def rotateZ(self, angle):
+        angle = angle * (180/math.pi)
+        sqrt2 = math.sqrt(2)
+        for point in enumerate(self.points):
+            newX = point[1][0] * math.cos(angle) - point[1][1] * math.sin(angle)
+            newY = point[1][1] * math.cos(angle) + point[1][0] * math.sin(angle)
+            newZ = point[1][2]
+            self.points[point[0]] = (newX, newY, newZ)
+
+    def rotateX(self, angle):
+        angle = angle * (180/math.pi)
+        sqrt2 = math.sqrt(2)
+        for point in enumerate(self.points):
+            newX = point[1][0]
+            newY = point[1][1] * math.cos(angle) - point[1][2] * math.sin(angle)
+            newZ = point[1][2] * math.cos(angle) + point[1][1] * math.sin(angle)
+            self.points[point[0]] = (newX, newY, newZ)
+
+    def rotateY(self, angle):
+        angle = angle * (180/math.pi)
+        sqrt2 = math.sqrt(2)
+        for point in enumerate(self.points):
+            newX = point[1][0] * math.cos(angle) - point[1][2] * math.sin(angle)
+            newY = point[1][1]
+            newZ = point[1][2] * math.cos(angle) + point[1][0] * math.sin(angle)
+            self.points[point[0]] = (newX, newY, newZ)
+    
     def __createTriangle(self, points):
         a, b, c = points[0], points[1], points[2]
         coords = [a[0], a[1], b[0], b[1], c[0], c[1]]
@@ -31,19 +59,31 @@ class Engine3D:
             self.image.delete(triangle)
 
     def render(self):
-        self.__clear()
         coords = []
+        
         for point in self.points:
             coords.append(self.__projectPoint(point))
             
         for triangle in self.triangles:
             self.__createTriangle((coords[triangle[0]], coords[triangle[1]], coords[triangle[2]]))
 
-        print('rendered')
-        self.window.after(1000, self.render)
+    ### Simple Rotation Animation ###
+    def animation(self):
+        self.__clear()
+        test.rotateY(0.0005)
+        test.rotateZ(0.0005)
+        self.render()
+        self.window.after(25, animation)
 
+### Example Use ###
 
 points = [(-1,-1,-1),(-1,-1,1),(-1,1,1),(-1,1,-1),(1,-1,-1),(1,-1,1),(1,1,1),(1,1,-1)]
-triangles = [(0,1,2),(0,2,3),(2,3,7),(2,7,6),(1,2,5),(2,5,6),(0,1,4),(1,4,5),(4,5,6),(4,6,7)]
+triangles = [(0,1,2),(0,2,3), (2,3,7),(2,7,6), (1,2,5),(2,5,6), (0,1,4),(1,4,5), (4,5,6),(4,6,7), (3,7,4),(4,3,0)]
+
 test = Engine3D(points, triangles)
-test.render()
+test.animation()
+
+###################
+
+
+
