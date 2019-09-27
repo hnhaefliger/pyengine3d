727302c6dfceed204fc0cecc101795c6dcf5cac0
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2019-09-27 09:17:14 +0200

    Add files via upload

---
+++ sharkExample.py
@@ -0,0 +1,35 @@
+### Shark ###
+
+import graphics
+
+points = []
+triangles = []
+
+with open('coords/SharkV.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        coords = line[:-2].split(' ')
+        points.append((float(coords[0]), float(coords[1]), float(coords[2])))
+    f.close()
+
+with open('coords/SharkT.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        coords = line[:-2].split(' ')
+        newCoords = []
+        for coord in coords[1:4]:
+            newCoords.append(int(coord))
+        triangles.append(newCoords)
+    f.close()
+
+test = graphics.Engine3D(points, triangles, scale=100)
+
+def animation():
+    test.clear()
+    test.rotateY(0.0005)
+    test.render('gray')
+    test.window.after(10, animation)
+
+animation()
+
+##############
