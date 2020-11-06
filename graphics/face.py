4c5f1f282296cf65c7d6dcd1a76e943f6ba60099
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2020-11-06 09:36:40 +0100

    Delete face.py

--- graphics/face.py
+++
@@ -1,8 +0,0 @@
-class Face:
-    def __init__(self, vertices):
-        #store point indexes
-        (a, b, c, color) = vertices
-        self.a = a
-        self.b = b
-        self.c = c
-        self.color = color
